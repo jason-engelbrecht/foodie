b0d4e3270cf714984353baef2a3c7fd58db7d41e
Jason Engelbrecht <jengelbrecht2@mail.greenriver.edu>
2019-05-29 18:34:45 -0400

    Database and classes setup

--- classes/Recipe.php
+++ classes/Recipe.php
@@ -1,12 +1,178 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: haro5
- * Date: 5/28/2019
- * Time: 10:06 PM
+ * Jason Engelbrecht & Alvin Nava
+ * 5.29.2019
+ * https://jengelbrecht.greenriverdev.com/it328/foodie
+ * Recipe Class
  */
 
+/**
+ * Class representing a recipe
+ *
+ * This class represents a basic recipe(abstract).
+ * @author Jason Engelbrecht & Alvin Nava
+ * @copyright 2019
+ */
 class Recipe
 {
+    private $_title;
+    private $_description;
+    private $_ingredients;
+    private $_instructions;
+    private $_time;
+    //private $_measure; children
+    private $_image;
+    private $_dateCreated;
+    private $_category;
+
+    /**
+     * Recipe constructor.
+     * @param $_title
+     * @param $_description
+     * @param $_time
+     * @param $_image
+     * @param $_dateCreated
+     * @param $_category
+     */
+    public function __construct($_title, $_description, $_time, $_image, $_dateCreated, $_category)
+    {
+        $this->_title = $_title;
+        $this->_description = $_description;
+        $this->_time = $_time;
+        $this->_image = $_image;
+        $this->_dateCreated = $_dateCreated;
+        $this->_category = $_category;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->_title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->_title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->_description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->_description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIngredients()
+    {
+        return $this->_ingredients;
+    }
+
+    /**
+     * @param mixed $ingredients
+     */
+    public function setIngredients($ingredients)
+    {
+        $this->_ingredients = $ingredients;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInstructions()
+    {
+        return $this->_instructions;
+    }
+
+    /**
+     * @param mixed $instructions
+     */
+    public function setInstructions($instructions)
+    {
+        $this->_instructions = $instructions;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->_time;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time)
+    {
+        $this->_time = $time;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->_image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->_image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreated()
+    {
+        return $this->_dateCreated;
+    }
+
+    /**
+     * @param mixed $dateCreated
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->_dateCreated = $dateCreated;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->_category;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category)
+    {
+        $this->_category = $category;
+    }
+
+
+
 
 }
--- model/Data.php
+++ model/Data.php
@@ -1,9 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: haro5
- * Date: 5/28/2019
- * Time: 10:07 PM
+/*
+ CREATE TABLE recipe (
+ recipe_id INT AUTO_INCREMENT PRIMARY KEY,
+ title VARCHAR(30) NOT NULL,
+ description TEXT NOT NULL,
+ ingredients TEXT NOT NULL,
+ instructions TEXT NOT NULL,
+ time VARCHAR(30) NOT NULL,
+ measure CHAR(10) NOT NULL,
+ image VARCHAR(50) NOT NULL,
+ date_created DATETIME NOT NULL,
+ category INT NOT NULL,
+ FOREIGN KEY(category) references category(category_id)
+ );
+
+ CREATE TABLE category (
+ category_id INT AUTO_INCREMENT PRIMARY KEY,
+ category VARCHAR(30)
+ );
+
+ INSERT INTO category(category)
+ VALUES('Breakfast');
+ INSERT INTO category(category)
+ VALUES('Lunch');
+ INSERT INTO category(category)
+ VALUES('Dinner');
+ INSERT INTO category(category)
+ VALUES('Dessert');
+ INSERT INTO category(category)
+ VALUES('Healthy');
+ INSERT INTO category(category)
+ VALUES('Baking');
+ INSERT INTO category(category)
+ VALUES('Drinks');
+ INSERT INTO category(category)
+ VALUES('Miscellaneous');
  */
 
 class Data
